7a9ad26a76cd938d29a7177515302d749af1aef7
Ben Lambell <blambell@plxs.com.au>
2022-09-16 16:51:54 +1000

    LG-0000: Add new function

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,11 @@
 def add(*args):
     return sum(*args)  # This change is a mistake, it should actually be sum(args)
+
+
+def multiply(*args):
+    if len(args) == 0:
+        raise ValueError("Cannot multiply an empty list of values.")
+    result = 1
+    for arg in args:
+        result *= arg
+    return result
